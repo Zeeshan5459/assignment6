98a10aad3eb14bad9afe873281c04b921fa4585a
Zeeshan5459 <zeeshankhan081082@gmail.com>
2024-08-08 19:44:00 +0500

    Update list.py

--- list.py
+++ list.py
@@ -1,36 +1,17 @@
-# List
-# Question: Write a function that takes a list of integers
-# and returns the sum of all the elements in the list.
+def sum_of_list-elements(lst):
+    return sum(lst)
+print(sum_of_list([3,5,8,10]))
 
-def sum_of_list(numbers):
-    total = 0
-    for num in numbers:
-        total += num
-    return total
-
-print(sum_of_list([3,5,8,10,17,31])) 
-
-# Tuple
-# Question: Write a function that takes a tuple of numbers
-# and returns the smallest number in the tuple.
-
+     ### question 2
 def smallest_number(*numbers):
     return min(numbers)
-print(smallest_number(* (4,6,3,9,11,22)))
-
-
-# Set
-# Question: Write a function that takes a set of numbers
-# and returns a new set with each element squared.
-
+print(smallest_number(* (4,6,3,9)))
+       ## question 3
 def set_numbers(*numbers):
     return {num**2 for num in numbers}
 
 print(set_numbers(*{2,4,6,7,9,13}))
-
-# Dictionary
-# Question: Write a function that takes a dictionary
-# and returns a list of all the keys in the dictionary.
+###    question 4
 
 def dic(person={
     "name":"M. Zeeshan",
