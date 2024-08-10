7f4993452f999f89d013515fce3ae18fd39e6cab
Zeeshan5459 <zeeshankhan081082@gmail.com>
2024-08-10 11:46:37 +0500

    Update list.py

--- list.py
+++ list.py
@@ -1,3 +1,4 @@
+    ### question 1
 def sum_of_list-elements(lst):
     return sum(lst)
 print(sum_of_list([3,5,8,10]))
@@ -5,19 +6,18 @@ print(sum_of_list([3,5,8,10]))
      ### question 2
 def smallest_number(*numbers):
     return min(numbers)
-print(smallest_number(* (4,6,3,9)))
+print(smallest_number(* (4,6,9)))
+
        ## question 3
 def set_numbers(*numbers):
     return {num**2 for num in numbers}
-
-print(set_numbers(*{2,4,6,7,9,13}))
+print(set_numbers(*{2,4,6,7}))
 ###    question 4
 
 def dic(person={
     "name":"M. Zeeshan",
     "Gender":"Male",
-    "Age":"19",
-    "City":"Karachi"
+    "Age":"19"
 }):
     return list(person.keys())
 print(dic())
